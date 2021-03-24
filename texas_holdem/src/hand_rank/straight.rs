3f4a2fa2787c6c74e2454358c197685d6edd58bd
datakdtk <0k.datak.ya@gmail.com>
2021-03-25 01:07:03 +0900

    Create functions to check hand rank for straight

---
+++ texas_holdem/src/hand_rank/straight.rs
@@ -0,0 +1,134 @@
+use crate::TotalHand;
+use super::{ BestFiveHand, HandRank };
+use playing_card::card:: { CardRank, NonJokerCard };
+
+
+pub(super) fn try_to_build_from_total_hand(hand: TotalHand) -> Option<BestFiveHand> {
+    let head_ranks_of_straight = hand.head_ranks_of_straight();
+    if head_ranks_of_straight.is_empty() {
+        return None;
+    }
+    let rank_of_head = head_ranks_of_straight[0];
+    let ranks = [
+        rank_of_head,
+        CardRank::new(rank_of_head.to_int() - 1),
+        CardRank::new(rank_of_head.to_int() - 2),
+        CardRank::new(rank_of_head.to_int() - 3),
+        CardRank::new(rank_of_head.to_int() - 4),
+    ];
+
+    let card_vec: Vec<NonJokerCard> = ranks.iter().map(|r| {
+        *hand.cards().iter().filter(|c| c.rank() == *r).next()
+             .expect(&format!("a card of rank {} is not found for straight", r.to_int()))
+    }).collect();
+
+    assert_eq!(5, card_vec.len());
+
+    let cards = [
+        card_vec[0],
+        card_vec[1],
+        card_vec[2],
+        card_vec[3],
+        card_vec[4],
+    ];
+    Some(BestFiveHand { cards, rank: HandRank::Straight })
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use playing_card::card:: { CardRank, NonJokerCard, Suit };
+
+    #[test]
+    fn returns_none_when_not_straight() {
+        let given_cards = [
+            NonJokerCard::new(Suit::Heart, CardRank::new(10)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(7)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(12)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(11)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(9)),
+        ];
+        let hand = TotalHand::new(&given_cards);
+        let result = try_to_build_from_total_hand(hand);
+        if let Some(_) = result {
+            panic!("Result is expected to be None");
+        }
+    }
+
+    #[test]
+    fn returns_some_when_straight() {
+        let given_cards = [
+            NonJokerCard::new(Suit::Heart, CardRank::new(10)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(8)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(12)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(11)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(9)),
+        ];
+        let hand = TotalHand::new(&given_cards);
+        let result = try_to_build_from_total_hand(hand);
+        if let None = result {
+            panic!("Result is expected not to be None");
+        }
+    }
+
+    #[test]
+    fn result_contains_ranks_of_straight() {
+        let given_cards = [
+            NonJokerCard::new(Suit::Heart, CardRank::new(10)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(8)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(12)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(11)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(9)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(1)),
+        ];
+        let hand = TotalHand::new(&given_cards);
+        let result = try_to_build_from_total_hand(hand);
+        let result_ranks = result.unwrap().value().card_ranks;
+        let expected_ranks = [
+            CardRank::new(12),
+            CardRank::new(11),
+            CardRank::new(10),
+            CardRank::new(9),
+            CardRank::new(8),
+        ];
+        assert_eq!(expected_ranks, result_ranks)
+    }
+
+    #[test]
+    fn result_contains_higher_straight_when_sequence_is_longer_than_5() {
+        let given_cards = [
+            NonJokerCard::new(Suit::Heart, CardRank::new(10)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(8)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(12)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(11)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(9)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(13)),
+        ];
+        let hand = TotalHand::new(&given_cards);
+        let result = try_to_build_from_total_hand(hand);
+        let result_ranks = result.unwrap().value().card_ranks;
+        let expected_ranks = [
+            CardRank::new(13),
+            CardRank::new(12),
+            CardRank::new(11),
+            CardRank::new(10),
+            CardRank::new(9),
+        ];
+        assert_eq!(expected_ranks, result_ranks)
+    }
+
+    #[test]
+    fn hand_rank_of_result_is_straight() {
+        let given_cards = [
+            NonJokerCard::new(Suit::Heart, CardRank::new(10)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(8)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(12)),
+            NonJokerCard::new(Suit::Heart, CardRank::new(11)),
+            NonJokerCard::new(Suit::Diamond, CardRank::new(9)),
+        ];
+        let hand = TotalHand::new(&given_cards);
+        let result = try_to_build_from_total_hand(hand);
+        assert_eq!(HandRank::Straight, result.unwrap().value().hand_rank)
+    }
+}
