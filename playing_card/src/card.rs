4f3a9df977fbb76740d2806e3ed6f1579d27b836
datakdtk <0k.datak.ya@gmail.com>
2021-03-26 00:24:40 +0900

    Associate card set creating functions to returned value types

--- playing_card/src/card.rs
+++ playing_card/src/card.rs
@@ -11,12 +11,40 @@ pub enum Card {
     Other(NonJokerCard),
 }
 
+impl Card {
+    pub fn new(suit: Suit, rank: CardRank) -> Self {
+        Self::Other( NonJokerCard{ suit, rank })
+    }
+
+    pub fn all_cards_with_one_joker() -> Vec<Card>
+    {
+        let mut cards : Vec<Card> = NonJokerCard::all().iter().map(|c| Card::Other(*c)).collect();
+        cards.push(Card::Joker);
+        cards
+    }
+    
+    pub fn all_cards_with_two_jokers() -> Vec<Card>
+    {
+        let mut cards : Vec<Card> = Self::all_cards_with_one_joker();
+        cards.push(Card::Joker);
+        cards
+    }
+}
+
 #[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
 pub struct NonJokerCard { suit: Suit, rank: CardRank }
 
 impl NonJokerCard {
     pub fn new(suit: Suit, rank: CardRank) -> Self {
         Self { suit, rank }
+    }
+
+    pub fn all() -> Vec<Self> {
+        Suit::all().iter().flat_map(|suit| {
+            CardRank::all().iter().map(|rank| {
+                NonJokerCard{ suit: *suit, rank: *rank }
+            }).collect::<Vec<NonJokerCard>>()
+        }).collect()
     }
 
     pub fn suit(self) -> Suit {
@@ -34,28 +62,6 @@ impl fmt::Display for NonJokerCard {
     }
 }
 
-pub fn all_non_joker_card() -> Vec<NonJokerCard> {
-    Suit::all().iter().flat_map(|suit| {
-        CardRank::all().iter().map(|rank| {
-            NonJokerCard{ suit: *suit, rank: *rank }
-        }).collect::<Vec<NonJokerCard>>()
-    }).collect()
-}
-
-pub fn all_cards_with_one_joker() -> Vec<Card>
-{
-    let mut cards : Vec<Card> = all_non_joker_card().iter().map(|c| Card::Other(*c)).collect();
-    cards.push(Card::Joker);
-    cards
-}
-
-pub fn all_cards_with_two_jokers() -> Vec<Card>
-{
-    let mut cards : Vec<Card> = all_cards_with_one_joker();
-    cards.push(Card::Joker);
-    cards
-}
-
 #[cfg(test)]
 mod test {
     use super::*;
@@ -63,12 +69,12 @@ mod test {
 
     #[test]
     fn all_non_joker_cards_are_52_cards() {
-        assert_eq!(52, all_non_joker_card().len());
+        assert_eq!(52, NonJokerCard::all().len());
     }
 
     #[test]
     fn all_non_joker_cards_are_all_different() {
-        let cards = all_non_joker_card();
+        let cards = NonJokerCard::all();
         let card_count = cards.len();
         let unique_set : HashSet<NonJokerCard> = cards.into_iter().collect();
         assert_eq!(card_count, unique_set.len());
@@ -76,12 +82,12 @@ mod test {
 
     #[test]
     fn all_cards_with_one_joker_are_53_cards() {
-        assert_eq!(53, all_cards_with_one_joker().len());
+        assert_eq!(53, Card::all_cards_with_one_joker().len());
     }
 
     #[test]
     fn all_cards_with_one_joker_are_all_different() {
-        let cards = all_cards_with_one_joker();
+        let cards = Card::all_cards_with_one_joker();
         let card_count = cards.len();
         let unique_set : HashSet<Card> = cards.into_iter().collect();
         assert_eq!(card_count, unique_set.len());
@@ -89,12 +95,13 @@ mod test {
 
     #[test]
     fn all_cards_with_two_jokers_are_54_cards() {
-        assert_eq!(54, all_cards_with_two_jokers().len());
+        assert_eq!(54, Card::all_cards_with_two_jokers().len());
     }
 
     #[test]
     fn all_cards_with_two_jokers_have_two_joker_cards() {
-        let joker_count = all_cards_with_two_jokers().into_iter().filter(|c| *c == Card::Joker).count();
+        let joker_count = Card::all_cards_with_two_jokers().into_iter()
+                             .filter(|c| *c == Card::Joker).count();
         assert_eq!(2, joker_count);
     }
 }
